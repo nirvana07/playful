172729caff7c642ed2c10eceacb4568c402bdbff
froz-ferre <alexej.ferre@gmail.com>
2018-10-02 23:49:25 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const tsc  = require('gulp-typescript');
 
+// Чтоб таска была синхронной и галп знал,
+// когда она завершилась - нужно возвращать значение
+// return gulp.src('index.js').pipe(uglify()).pipe(gulp.dest('dist'));
 
 gulp.task('default', function() {
     // Do whatever u want!

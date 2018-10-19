077e2f76dfcffb52736dcf9833979c53c9f57ed8
froz-ferre <alexej.ferre@gmail.com>
2018-10-19 23:09:44 +0300

    modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NotFoundComponent],
+  declarations: [NotFoundComponent, ModalComponent],
   exports: [NotFoundComponent]
 })
 export class SharedModule { }
